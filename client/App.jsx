6c559e30eb58d9aa6a0c6cc184473dbcf9c55a73
Jeff Maughan <geophray@gmail.com>
2020-04-30 14:26:41 -0700

    Add rendered app to index.html

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+console.log('Webpack is watching for changes!');
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  render(){
+
+  render() {
     return(
       <div>My app is rendering!</div>
     );
