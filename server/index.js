7078dcaedc1e81fcda4be119cd267d04742c167a
Jeff Maughan <geophray@gmail.com>
2020-04-30 12:24:49 -0700

    Use path.join instead of path.resolve

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3003;
 // console.log(__dirname);
-app.use(express.static(path.resolve(__dirname, '/../public')));
+app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
