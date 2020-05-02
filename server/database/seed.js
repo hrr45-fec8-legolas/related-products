2b9f3f3a42cd2c2ebe62ff04ac3377c6629d1ff8
Jeff Maughan <geophray@gmail.com>
2020-05-01 19:55:41 -0700

    Link each product to two categories
    Also drops all data to completely refresh the tables each time the seed
    script is run.

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,8 +1,12 @@
 const faker = require('faker');
 const db = require('./index.js');
+const drop = require('./drop.js');
 
 async function seedDatabase() {
+  await drop();
   await db.connection.sync();
+
+  // Generate categories
   let cats = new Set();
   while (cats.size < 10) {
     const name = faker.random.word();
@@ -10,7 +14,9 @@ async function seedDatabase() {
   }
   cats = Array.from(cats);
   cats = cats.map((cat) => ({ name: cat }));
-  // const categories = await db.Category.bulkCreate(cats, { ignoreDuplicates: true });
+  await db.Category.bulkCreate(cats, { ignoreDuplicates: true });
+
+  // Generate newProducts data
   const images = ['img1', 'img2', 'img3'];
   const imglen = images.length;
   const newProds = [];
@@ -23,15 +29,24 @@ async function seedDatabase() {
       prime: Math.floor(Math.random() * 2),
       imageUrl: i > imglen ? images[i % imglen] : images[i],
       numReviews: faker.random.number(),
-      avgRating: (Math.floor(Math.random() * 11)) / 2,
+      avgRating: (Math.floor((Math.random() * 6) + 5)) / 2,
     });
     i += 1;
   }
-  const products = await db.Product.bulkCreate(newProds);
-  // console.log(newProds);
+  await db.Product.bulkCreate(newProds);
 
-  // Generate newProducts data
-  // Use Products.bulkCreate(newProducts) to add all new products to db
-};
+  // Create associations between products/categories
+  for (let j = 1; j < 101; j += 1) {
+    const prodCats = new Set();
+    const associations = [];
+    while (prodCats.size < 2) {
+      prodCats.add(Math.ceil(Math.random() * 10));
+    }
+    prodCats.forEach((categoryId) => {
+      associations.push({ productId: j, categoryId });
+    });
+    db.ProductCategory.bulkCreate(associations);
+  }
+}
 
 seedDatabase();
