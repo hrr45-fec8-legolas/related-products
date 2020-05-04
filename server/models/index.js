6bcc2229130df8f7190a024856031ecf531c523d
Jeff Maughan <geophray@gmail.com>
2020-05-04 11:40:57 -0700

    Refactor get requests to use mysql

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,11 @@ const db = require('../database');
 
 module.exports = {
   products: {
-    getRelated(id, callback) {
-      return db.Product.findAll()
-        .then((products) => {
-          callback(null, products);
-        })
-        .catch((err) => {
-          callback(err);
-        });
+    getRelated(productId, callback) {
+      const sql = 'SELECT p.*, c.name AS category_name FROM products AS p INNER JOIN productCategories AS pcj ON pcj.id_products = p.id INNER JOIN categories AS c ON pcj.id_categories = c.id WHERE pcj.id_categories IN (SELECT pcj.id_categories FROM productCategories AS pcj INNER JOIN products AS p ON p.id = pcj.id_products WHERE p.productId = ?) ORDER BY avgRating DESC, numReviews DESC LIMIT 20';
+      db.query(sql, [productId], (err, results) => {
+        callback(err, results);
+      });
     },
     addNew(params, callback = () => {}) {
       const sql = 'INSERT INTO products (productId, name, price, prime, imageUrl, numReviews, avgRating) VALUES (?, ?, ?, ?, ?, ?, ?)';
@@ -20,8 +17,11 @@ module.exports = {
   },
 
   categories: {
-    getAll() {
-      return db.Category.findAll();
+    getAll(callback) {
+      const sql = 'SELECT id FROM categories ORDER BY id ASC';
+      db.query(sql, (err, results) => {
+        callback(err, results);
+      });
     },
     addNew(category, callback = () => {}) {
       const sql = 'INSERT INTO categories (name) VALUES (?)';
