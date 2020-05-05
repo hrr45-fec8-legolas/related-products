53c5ed6026a6351b72e02f0f5880d3c93cd6f7ae
Jeff Maughan <geophray@gmail.com>
2020-05-05 16:03:26 -0700

    Get product id from url and pass to get request for data

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import PageCount from './components/PageCount';
 import Arrow from './components/Arrow';
@@ -34,6 +35,25 @@ class App extends React.Component {
     };
     this.previous = this.previous.bind(this);
     this.next = this.next.bind(this);
+    this.getRelatedProducts = this.getRelatedProducts.bind(this);
+  }
+
+  componentDidMount() {
+    const params = new URLSearchParams(document.location.search.substring(1));
+    const id = params.get('id');
+    this.getRelatedProducts(id);
+  }
+
+  getRelatedProducts(id) {
+    axios.get(`/api/related_products/${id}`)
+      .then((results) => {
+        this.setState((state) => {
+          return {
+            products: results,
+          };
+        });
+      })
+      .catch((err) => console.error('Failed to load product data. => ', err));
   }
 
   previous() {
