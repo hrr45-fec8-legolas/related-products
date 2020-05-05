b989f0642f29894cbbf430557377ccac022330ae
Jeff Maughan <geophray@gmail.com>
2020-05-05 13:40:41 -0700

    Add basic html structure of ProductListItem component

--- client/components/ProductListItem.jsx
+++ client/components/ProductListItem.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 import ProductFeedback from './ProductFeedback';
+import Prime from './Prime';
 
 const ProductListItem = ({ product }) => {
   return (
     <div className="single-product">
       <ProductFeedback />
-      <div>{ product }</div>
+      <img src={product.imageUrl} alt={product.name} />
+      <div className="product-title">{product.name}</div>
+      <div className="product-ratings">
+        <span className="star-rating">Insert Star Rating</span>
+        <span className="total-reviews">{product.numReviews}</span>
+      </div>
+      <div className="product-pricing">
+        <span className="price">{product.price}</span>
+        <Prime isPrime={product.prime} />
+      </div>
     </div>
   );
 };
