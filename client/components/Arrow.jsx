a7963f81814c020932612f70655ea5145deea3b6
Jeff Maughan <geophray@gmail.com>
2020-05-08 13:32:15 -0700

    Add styling/stylesheet for arrow buttons

--- client/components/Arrow.jsx
+++ client/components/Arrow.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
+import style from '../css/arrow.css';
+
 const Arrow = ({ direction, nextPane }) => (
-  <button type="button" onClick={nextPane}>{direction}</button>
+  <div className={style['arrow-wrapper']}>
+    <button type="button" onClick={nextPane}><i className={style['slide-' + direction]} /></button>
+  </div>
 );
 
 Arrow.propTypes = {
