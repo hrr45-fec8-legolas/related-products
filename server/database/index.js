870933190ca3dba5a2a0a6bac4cba9d01ac100f2
Jeff Maughan <geophray@gmail.com>
2020-05-15 18:05:04 -0700

    Add docker env vars

--- server/database/index.js
+++ server/database/index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const db = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: $DB_HOST || process.env.DB_HOST || 'localhost',
+  user: $DB_USER || process.env.DB_USER || 'root',
+  password: $DB_PASS || process.env.DB_PASS || '',
+  database: $DB_NAME || process.env.DB_NAME || 'fec8_related_products',
 });
 
 module.exports = db;
