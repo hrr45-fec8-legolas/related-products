e70f52aadfd23ee2f17e1300910dc3cc399ad302
Jeff Maughan <geophray@gmail.com>
2020-05-16 17:53:19 -0700

    Split app.js out of index.js for supertest

---
+++ server/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,5 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
+const app = require('./app.js');
 
-const routes = require('./routes');
-
-const app = express();
 const port = process.env.PORT || 3003;
-
-app.use(express.static(path.join(__dirname, '/../public')));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', routes);
 
 app.listen(port);
