3390efa799fd490e9c2b00ca9fd5ec6b28ce93fa
Jeff Maughan <geophray@gmail.com>
2020-05-16 17:54:36 -0700

    Add new tests for api endpoint

---
+++ spec/endpoints.spec.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const app = require('../server/app.js');
+
+describe('Test the related_objects endpoint', () => {
+  test('Should return json for get request to /?id=1', (done) => {
+    request(app)
+      .get('/api/related_products/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(done)
+  });
+
+  test('Should not contain object id of 1 in results for request to /?id=1', (done) => {
+    request(app)
+    .get('/api/related_products/1')
+      .expect(function(res) {
+        const productIds = res.body.map(obj => {
+          return obj.productId;
+        });
+        if(productIds.includes(1)) throw new Error('Related products should not include the current product.');
+      })
+      .end(done)
+  });
+});
