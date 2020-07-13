0778834fe93609c0bea2708f58a9f73766c60b7a
Jeff Maughan <geophray@gmail.com>
2020-07-13 13:37:09 -0600

    Add default db values

--- server/database/index.js
+++ server/database/index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const db = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'related_products',
 });
 
 module.exports = db;
